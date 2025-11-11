a53636ee7172d46ed411fe003fd010c1635a9e05
ChandrikaKaspadi <chandrikacc26@gmail.com>
2025-11-11 17:45:07 +0530

    Practice

---
+++ SeleniumBatch4/src/test/java/chandrikapractice/SampleForGitHubPractice.java
@@ -0,0 +1,11 @@
+package chandrikapractice;
+
+import org.testng.annotations.Test;
+
+public class SampleForGitHubPractice {
+	@Test
+	public void toPracticeGitHub() {
+		System.out.println("Hi Pramod");
+	}
+
+}
