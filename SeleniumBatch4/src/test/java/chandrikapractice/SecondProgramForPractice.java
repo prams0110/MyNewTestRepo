3c0d000447d84fa50d30893b668b237dd8d6caa0
ChandrikaKaspadi <chandrikacc26@gmail.com>
2025-11-11 18:02:43 +0530

    2nd Commit for practice

---
+++ SeleniumBatch4/src/test/java/chandrikapractice/SecondProgramForPractice.java
@@ -0,0 +1,11 @@
+package chandrikapractice;
+
+import org.testng.annotations.Test;
+
+public class SecondProgramForPractice {
+@Test
+	public void sampleProgram2() {
+		System.out.println("Hi Pramod");
+		
+	}
+}
