60cd3df86ea3a993347fa6ce694115eb01d0c93f
ChandrikaKaspadi <chandrikacc26@gmail.com>
2025-11-11 18:22:07 +0530

    Third commit

---
+++ SeleniumBatch4/src/test/java/chandrikapractice/ThirdSampleProgram.java
@@ -0,0 +1,12 @@
+package chandrikapractice;
+
+import org.testng.annotations.Test;
+
+public class ThirdSampleProgram {
+	@Test
+	public void practice() {
+		System.out.println("pushing code to new Branch");
+		
+	}
+
+}
